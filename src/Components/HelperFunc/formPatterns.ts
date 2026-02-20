2bd707e7082f8f71d30292a8e3870834834ff32b
Tamara Najdovska <tamaranajdovska5@gmail.com>
2026-02-20 22:02:29 +0100

    fixed regex paterns for validation

--- src/Components/HelperFunc/formPatterns.ts
+++ src/Components/HelperFunc/formPatterns.ts
@@ -1,15 +1,17 @@
-export const mkdPattern: FormRegexPatterns={
-    namePattern: new RegExp(/[а-шА-Ш]{2,30}/g),
-    addressPattern: new RegExp(/^[А-Ша-ш][А-Ша-ш0-9\s,.'-/]{5,50}$/) ,
+export const mkdLetters = 'А-Ша-шЃѓЌќЅѕЏџЉљЊњЈј';
+
+export const mkdPattern: FormRegexPatterns = {
+    namePattern: new RegExp(`[${mkdLetters}]{2,30}`, 'g'),
+    addressPattern: new RegExp(`^[${mkdLetters}0-9\\s,.'-/]{1,50}$`),
     socialNumber: new RegExp(/^[0-9]*$/),
     email: new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/),
     phone: new RegExp(/^\+?[0-9\s\-/]{9,20}$/)
-}
+};
 
 
 export const albPatterns: FormRegexPatterns={
-    namePattern: new RegExp(/[a-zA-Z]{2,50}/g),
-    addressPattern: new RegExp(/^[A-Za-z][A-Za-z0-9\s,.'-/]{5,50}$/),
+    namePattern: new RegExp(/[a-zA-Z]{2,30}/g),
+    addressPattern: new RegExp(/^[A-Za-z0-9\\s,.'-/]{1,50}$/),
     socialNumber: new RegExp(/^[0-9]*$/),
     email: new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/),
     phone: new RegExp(/^\+?[0-9\s\-/]{9,20}$/)
